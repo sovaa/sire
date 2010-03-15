3ce8a170af4ce4e5b2b2c4f926148e84c9de696f
Oscar Eriksson <oscar.eriks@gmail.com>
2010-03-15 18:36:15 +0000

    forgot info.py

---
+++ sire/info.py
@@ -0,0 +1,58 @@
+
+# Prints formated table with info about items with specified IDs.
+def info(ids, dests = None):
+    from sire.helpers import config_value, is_valid_id, get_info_from_id, cnr_parser, sort
+    import sys
+
+    ids = cnr_parser(ids, "id")
+    dests = cnr_parser(dests, "cat")
+
+    # [id, score, title, category, date_added, time_in_category]
+    strlens = [1,1,1,1,1,1]
+    items = []
+
+    # go through all specified IDs
+    for id in ids:
+        result = get_info_from_id(id)
+        if result:
+            items.append(result)
+
+    # found nothin', error/warning/whatever already shown if so
+    if not items:
+        return
+
+    # calculate each columns max width if not in skip list
+    for item in items:
+        # if destination cateogries are specified, and this item's not in it, continue
+        if dests and item[3] not in dests:
+            continue
+        for i in range(len(strlens)):
+            if len(str(item[i])) > strlens[i]:
+                strlens[i] = len(str(item[i]))
+
+    # calculate spaces between column headers and print them
+    width = 0
+    labels = ['ID  ', 'SCORE  ', 'TITLE  ', 'CATEGORY  ', 'DATE ADDED  ', 'TIME IN CATEGORY  ']
+    for i in range(len(labels)):
+        spaces = max(strlens[i], len(labels[i])) - len(labels[i]) + 1
+        width += spaces + len(labels[i])
+        sys.stdout.write(labels[i] + ' '*spaces + '| ')
+    width = width + 2*len(labels) - 1
+    print '\n' + '-'*width
+    #print '\n' + '-'*(width + 2*len(labels) - 1)
+
+    items = sort(items)
+
+    # calculate spaces between column items and print them
+    for item in items:
+        # if destination cateogries are specified, and this item's not in it, continue
+        if dests and item[3] not in dests:
+            continue
+        for i in range(len(strlens) - 1):
+            spaces = max(strlens[i] - len(str(item[i])), len(labels[i]) - len(str(item[i]))) + 1
+            sys.stdout.write('%s%s| ' % (item[i], ' '*spaces))
+        # this is the time it's been in this category
+        print '%sy %sd %sh %sm %ss' % (item[5][0], item[5][1], item[5][2], item[5][3], item[5][4])
+    print '-'*width
+    return
+
