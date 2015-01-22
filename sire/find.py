89cd9ce92727c98dffdbef8ac3ebf0fc4586479d
Oscar Eriksson <oscar.eriks@gmail.com>
2015-01-22 16:54:58 +0800

    pretty format search results, refactored some search code

--- sire/find.py
+++ sire/find.py
@@ -1,38 +1,93 @@
-
-
+from sire.helpers import config_value
 
 # Do an approximate string search using the Apse package.
 def approxsearch(edits, sstr):
-    from sire.helpers import config_value, replace_all, text_warning, text_note, format_text_out
+    from sire.helpers import text_warning, format_text_out
     from sire.printer import format_category_out
-    from fuzzywuzzy import fuzz # fuzzy string matching
-    import sire.dbman as dbman
     from sire.shared import opt
     from sire.misc import Misc
 
+    show_categories = config_value('find.showcats') == '1' and opt.get('category') is not False
+    edits = determine_edits(edits)
+    results = search(edits, sstr)
+    sort_results(results, show_categories)
+
+    if not results:
+        text_warning("No matches found!")
+        return
+
+    previous_category = None
+    for result in results:
+        item_id, ratio, title, category = result[0], str(result[2]) + '%', result[1], result[3]
+        spacer_id = ' '*(5 - len(str(item_id)))
+        spacer_ratio = ' '*(6 - len(ratio))
+
+        if show_categories:
+            if should_show_category_header(previous_category, category):
+                if previous_category is not None:
+                    print("")
+                previous_category = category
+                print(format_category_out(category))
+                print_columns()
+
+        # Showing ID when listing is optional.
+        if config_value("general.showid") == '0' or opt.get('id') is False:
+            print(format_text_out(title))
+        else:
+            print("%s%s%s %s | %s%s | %s" %
+                  (Misc.C['bold'], item_id, Misc.C['default'],
+                  spacer_id, ratio, spacer_ratio, format_text_out(title)))
+
+
+def should_show_category_header(previous_category, category):
+    if previous_category is None:
+        return True
+    if category is not previous_category:
+        return True
+    return False
+
+
+def sort_results(results, show_categories):
+    if not results:
+        return
+    if show_categories:
+        results.sort(lambda x, y: cmp(x[2], y[2]))
+        results.sort(lambda x, y: cmp(x[3], y[3]))
+        results.reverse()
+    else:
+        results.sort(lambda x, y: cmp(x[2], y[2]))
+        results.reverse()
+
+
+def determine_edits(edits):
+    if edits is 0:
+        if config_value("find.edits"):
+            edits = int(config_value("find.edits"))
+        else:
+            edits = 18  # default
+    if edits < 0:
+        edits = 0
+    elif edits > 100:
+        edits = 100
+    return edits
+
+
+def search(edits, sstr):
+    from sire.helpers import replace_all
+    from fuzzywuzzy import fuzz  # fuzzy string matching
+    import sire.dbman as dbman
+
+    db = dbman.get_items()
     results = []
     reps = {
         '.': ' ',
         '_': ' ',
         '-': ' '
     }
-
-    if edits is 0:
-        if config_value("find.edits"):
-            edits = int(config_value("find.edits"))
-        else:
-            edits = 18 # default
-    if edits < 0:
-        edits = 0
-    elif edits > 100:
-        edits = 100
-
-    db = dbman.get_items()
     for search in [sstr]:
         # allow at most 'edits' edits
         min_ratio = 100 - edits
-
-        for key in db: 
+        for key in db:
             value = key[1]
             item_id = key[0]
             category = key[2]
@@ -41,34 +96,9 @@ def approxsearch(edits, sstr):
             res = (item_id, value, ratio, category)
             if found and value not in results:
                 results.append(res)
+    return results
 
-    if not results:
-        text_warning("No matches found!")
-        return
 
-    showcats = config_value('find.showcats') == '1' and opt.get('category') is not False
-    text_note("Match found. Best match at the top.")
-
-    if showcats:
-        results.sort(lambda x, y: cmp(x[2], y[2]))
-        results.sort(lambda x, y: cmp(x[3], y[3]))
-        results.reverse()
-    else:
-        results.sort(lambda x, y: cmp(x[2], y[2]))
-        results.reverse()
-
-    previous_category = None
-    for result in results:
-        if showcats:
-            if previous_category is None or result[3] != previous_category:
-                previous_category = result[3]
-                print '\n' + format_category_out(result[3])
-
-        # Make titles aligned.
-        spacer = ' '*(5 - len(str(res[0])) + 3 - len(str(result[2])))
-
-        # Showing ID when listing is optional.
-        if config_value("general.showid") == '0' or opt.get('id') is False:
-            print result[1]
-        else:
-            print "%s%s%s %s (ratio %s): %s" % (Misc.C['bold'], result[0], Misc.C['default'], spacer, result[2], format_text_out(result[1]))
+def print_columns():
+    print("ID     | MATCH  | TITLE")
+    print("-------+--------+----------------------------------------------------")
